58ed74525707d8ca74b8225155f912fa037e55ce
abdulwariskhan <hellokhan.ch@gmail.com>
2024-11-11 23:04:48 +0100

    meet chat boot

---
+++ src/ChatBot.java
@@ -0,0 +1,12 @@
+public class ChatBot {
+
+    public static void main(String[] args) {
+
+        String botName = "Waris";
+        int brithYear = 1996;
+        System.out.println("Hello! My name is " + botName);
+        System.out.println("I was created in "+brithYear);
+
+
+    }
+}
