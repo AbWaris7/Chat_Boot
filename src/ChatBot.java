6d014f267cde7bd2222430a85f0091f2d71efe7f
abdulwariskhan <hellokhan.ch@gmail.com>
2024-11-12 21:30:17 +0100

    Let the chat boot guess your age

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -15,7 +15,15 @@ public class ChatBot {
         String guestName = input.nextLine();
         System.out.println("What a great name you have, "+guestName);
 
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
+        int remainder3 = input.nextInt();
+        int remainder5 = input.nextInt();
+        int remainder7 = input.nextInt();
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.println("Your age is "+age+"; that's a good time to start programming!");
 
 
     }
